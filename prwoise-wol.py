3c72ca44238795455388545f8a0ed58f78a9f944
LucEast <lucas.ostmann@gmail.com>
2022-10-17 16:53:17 +0200

    updated regex

--- prwoise-wol.py
+++ prwoise-wol.py
@@ -20,7 +20,7 @@ except:
 
 
 def ip_validation(current):
-    if not re.match('^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$', current):
+    if not re.match('"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"gm', current):
         raise errors.ValidationError('', reason="Your BC-Address isn't valid!")
     return True
 
